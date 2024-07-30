1e3cf2d56a0d8c7ab7f33a4127b31d8511bd86bf
Arif-cloud-js <m.arifalam450@gmail.com>
2024-07-30 12:49:18 +0500

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json({ extended: true })); 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());  // Use the cors middleware
+app.use(cors());  
 
 /**
  * Reads previously authorized credentials from the save file.
